3f0d55fda2da7d4decd66cf35d59376650dc646c
Marlon Prudente <marlonoliveira@alunos.utfpr.edu.br>
2018-05-25 15:25:01 -0300

    alteracoes lab

--- driverlib/drivertimer.c
+++ driverlib/drivertimer.c
@@ -3,6 +3,7 @@
 #include "inc/hw_ints.h"
 #include "inc/hw_nvic.h"
 #include "inc/hw_types.h"
+#include "driverlib/gpio.h"
 #include "driverlib/debug.h"
 #include "driverlib/interrupt.h"
 #include "driverlib/systick.h"
@@ -10,32 +11,21 @@
 #include "inc/tm4c1294ncpdt.h" // CMSIS-Core
 #include "driverlib/timer.h"
 
-
 void IniciaTimer(){
-  TIMER0_CTL_R  = 0x00000000; //desliga o timer
+    
+  SYSCTL_RCGCTIMER_R = 0x00000001; //Habilitar GPTM0
+  TIMER0_CTL_R  = 0x00000000; //desliga o timer/borda de subida
   TIMER0_CFG_R  = 0x00000004; // usar timer 16 bits
-  TIMER0_TAMR_R = 0x00000013; //TACMR
-//  TIMER0_TAMR_R |= (1<<0); //TAMR = 0x3
-//  TIMER0_TAMR_R |= (1<<1); //TAMR = 0x3
-//  TIMER0_CTL_R &= (0<<2) ; //BORDA SUBIDA
-//  TIMER0_CTL_R &= (0<<3) ; //BORDA SUBIDA
+  TIMER0_TAMR_R = 0x00000013; //Modo compare e capture
   TIMER0_TAMATCHR_R = 0xFFFFFFFE; //
   TIMER0_TAPMR_R  = 0xFFFFFFFE; //
   TIMER0_TAPR_R  = 0xFFFFFFFF; //
-  TIMER0_TAILR_R = 0xFFFFFFFF; //
-  //TIMER0_CTL_R  &= (1<<0); //liga o timer
-  
-  //SysCtlPeripheralEnable(TIMER0_CFG_R); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
-  
-  //while(!SysCtlPeripheralReady(TIMER0_CFG_R)); // Aguarda final da habilitação
-  
+  TIMER0_TAILR_R = 0xFFFFFFFF; // 
 
 };
 
 void ReiniciaContador(){
   TIMER0_CTL_R  = 0x00000000; //desliga o timer
-  TIMER0_CTL_R &= (0<<2) ; //BORDA SUBIDA
-  TIMER0_CTL_R &= (0<<3) ; //BORDA SUBIDA
   TIMER0_TAMATCHR_R &= 0xFFFFFFFE; //
   TIMER0_TAPMR_R  &= 0xFFFFFFFE; //
   TIMER0_TAPR_R  &= 0xFFFFFFFF; //
--- laboratorio2/src/main.c
+++ laboratorio2/src/main.c
@@ -9,28 +9,40 @@
 #include "driverlib/systick.h"
 #include "driverlib/drivertimer.h"
 #include "driverlib/uartstdio.h"
+#include "inc/hw_types.h"
 
 #define PERIODO 60001
 #define DELTA 3000
+#define TIMER_A 0x000000ff
+#define TIMER_O_TAV 0x00000050
 
 uint32_t ui32SysClock ;
 uint8_t LED_D1 = 0;
 int32_t LED_D4 = PERIODO;
 uint8_t updn = 1;
+int32_t contador;
+int32_t contador0;
+
+// SysTick_Handler
 void SysTick_Handler(void){
     GPIOPinWrite(GPIO_PORTN_BASE, GPIO_PIN_1, LED_D1);
     LED_D1 ^= GPIO_PIN_1;
-    //ReiniciaContador();
-} // SysTick_Handler
+    contador=TimerValueGet(TIMER0_BASE, TIMER_A); 
+    HWREG(TIMER0_BASE + TIMER_O_TAV) = 0; //limpa GPTMTAR
+    ReiniciaContador();
+} 
 
-//void Periodo1(){
-//SysTickPeriodSet(25000000); // f = 25MHz / t = 1 s
-//int32_t contador = TIMER0_TAR_R;
-//
-//}
-//void Periodo1ms(){
-//SysTickPeriodSet(25000); // f = 25MHz / t = 1ms
-//}
+//Configura Int Systick para 1s
+void Periodo1(){
+    SysTickPeriodSet(25000000); // f = 25MHz / t = 1 s
+   // contador = 0;
+}
+
+//Configura Int Systick para 1ms
+void Periodo1ms(){
+    SysTickPeriodSet(25000); // f = 25MHz / t = 1ms;
+  //  contador = 0;
+}
 
 void ConfigUART(void){
   
@@ -43,24 +55,28 @@ void ConfigUART(void){
   UARTStdioConfig(0, 115200, ui32SysClock);
   
 }
-
 void main(void){
  ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
                                               SYSCTL_OSC_MAIN |
                                               SYSCTL_USE_PLL |
                                               SYSCTL_CFG_VCO_480),   
                                               120000000); // PLL em 120MHz
+   //CONFIGURA PD0 COMO TIMER0
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_TIMER0);
+   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOD); // Habilita GPIO D (T0CCP0 = PD0)   
+   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPIOD)); // Aguarda final da habilitação  
+   GPIOPinTypeTimer(GPIO_PORTD_BASE, GPIO_PIN_0);  
+   GPIOPinConfigure(GPIO_PD0_T0CCP0);
+   
+   //HABILITA INTERRUPÇÕES
+   IntMasterEnable();
+   
    IniciaTimer();
+   ConfigUART(); 
    SysTickEnable();
-   SysTickPeriodSet(25000000);   
-   ConfigUART();
-  //UARTprintf("TESTE");
- 
-  //Periodo1();
-  
-  
+   SysTickPeriodSet(25000000);  
+   
   SysCtlPeripheralEnable(SYSCTL_PERIPH_GPION); // Habilita GPIO N (LED D1 = PN1, LED D2 = PN0)
-  
   while(!SysCtlPeripheralReady(SYSCTL_PERIPH_GPION)); // Aguarda final da habilitação
   
   GPIOPinTypeGPIOOutput(GPIO_PORTN_BASE, GPIO_PIN_0 | GPIO_PIN_1); // LEDs D1 e D2 como saída
@@ -77,11 +93,18 @@ void main(void){
 
   while(1){
     
-    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) != GPIO_PIN_0) // Testa estado do push-button SW1
-        UARTprintf("TESTE1");//Periodo1();
+    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_0) != GPIO_PIN_0) // Testa estado do push-button SW1        
+        Periodo1();
     
-    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) != GPIO_PIN_1) // Testa estado do push-button SW2
-       UARTprintf("TESTE2");// Periodo1ms();    
-      
+    if(GPIOPinRead(GPIO_PORTJ_BASE, GPIO_PIN_1) != GPIO_PIN_1) // Testa estado do push-button SW2        
+        Periodo1ms(); 
+        
+      if(contador != contador0){
+            UARTprintf("\rT1:%i Hz", contador);
+      }    
+        else
+          UARTprintf("\rT1:%i kHz", contador);
+          contador0=contador;      
+    }
   };
-} // main
+ // main
